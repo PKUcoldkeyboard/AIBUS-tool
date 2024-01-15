18bafaef8a6f21b382c46f1096c79c85d5aae3fe
cuterwrite <158505862@qq.com>
2024-01-15 09:21:32 +0000

    fix: 🐛 Fix old code due to AIBUSX update

--- func.py
+++ func.py
@@ -1,56 +1,59 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from __future__ import annotations
+
 import time
+import traceback
 import warnings
+
+from selenium.webdriver.common.by import By
 warnings.filterwarnings('ignore')
+
 
 def login(driver, username, password, retry=0):
     if retry == 3:
         raise ValueError('login failed.')
-    
+
     print('login...')
     url = 'https://www.aibusx.com/pages/login'
     driver.get(url)
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div/div/div/form/button')))
-    driver.find_element_by_id('email').send_keys(username)
+    # WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'MuiButton-sizeLarge')))
+    time.sleep(5)
+    driver.find_element(By.ID, 'email').send_keys(username)
     time.sleep(0.1)
-    driver.find_element_by_id('auth-login-password').send_keys(password)
+    driver.find_element(By.ID, 'auth-login-password').send_keys(password)
     time.sleep(0.1)
-    driver.find_element_by_xpath('/html/body/div/div[2]/div/div/div/div/form/button').click()
+    driver.find_elements(By.CLASS_NAME, 'MuiButton-sizeLarge')[0].click()
     try:
-        # WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, ':r1:')))
         print('login succesfully.')
-    except:
+    except Exception as e:
         print('Retrying...')
         login(driver, username, password, retry+1)
-        
+
     time.sleep(3)
-        
-        
+
+
 # 签到
 def checkin(driver):
     try:
-        wait = WebDriverWait(driver, 10)
-        
-        # 使用WebDriverWait等待元素可见
-        button = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'MuiLoadingButton-root')))
-        
+        time.sleep(10)
+        button = driver.find_elements(
+            By.CLASS_NAME, 'MuiButton-textCheckinBtn')[0]
+        print(button.get_dom_attribute('class'))
         # 检查button是否有效，可点击
         if button.is_enabled():
             button.click()
         else:
-            raise RuntimeError("Button is disabled or not clickable")
+            raise RuntimeError('Button is disabled or not clickable')
         time.sleep(3)
         print('签到成功！')
     except Exception as e:
         print('签到失败！')
         traceback.print_exc()
-        
+
 
 def run(driver, username, password):
     login(driver, username, password)
     checkin(driver)
-    
+
+
 if __name__ == '__main__':
     pass
--- main.py
+++ main.py
@@ -1,41 +1,40 @@
-import env_check
-from configparser import ConfigParser
-from selenium import webdriver
-from func import *
+from __future__ import annotations
+
+import argparse
 import warnings
-import sys
-import os
+from configparser import ConfigParser
+
+from selenium import webdriver
+
+import env_check
+from func import *
 warnings.filterwarnings('ignore')
 
-def sys_path(browser):
-    path = f'./{browser}/bin/'
-    if sys.platform.startswith('win'):
-        return path + f'{browser}.exe'
-    elif sys.platform.startswith('linux'):
-        return path + f'{browser}-linux'
-    elif sys.platform.startswith('darwin'):
-        return path + f'{browser}'
-    else:
-        raise OSError('Unknown OS Type')
-    
+
 def go(config):
     conf = ConfigParser()
     conf.read(config, encoding='utf-8')
     username, password = dict(conf['login']).values()
-    
+
     run(driver, username, password)
 
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='AIBUS-tool')
+    parser.add_argument('--driver_path', type=str, default='/usr/bin/chromedriver',
+                        help='absolute path of your chromedriver or chromedrier.exe, \
+                            default value=/usr/bin/chromedriver')
+    args = parser.parse_args()
+
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    # chromedriver = sys_path('chromedriver')
-    # 用 action 自带的 chromedriver
-    chromedriver = '/usr/bin/chromedriver'
-    os.environ["webdriver.chrome.driver"] = chromedriver
-    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriver)
-    print(f"Chromedriver is ready.")
+
+    chromedriver = args.driver_path
+    service = webdriver.ChromeService(executable_path=chromedriver)
+    driver = webdriver.Chrome(options=chrome_options, service=service)
+    print(f'Chromedriver is ready.')
     go('config.ini')
     driver.quit()
