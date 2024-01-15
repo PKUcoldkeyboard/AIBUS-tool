889e279a97a76fc499b681081acb37a7377e67c3
cuterwrite <158505862@qq.com>
2024-01-15 10:38:22 +0000

    feat: 🎸 新增了验证码识别功能，解决签到时弹出验证码的问题

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # Aibus-tool
 自动打卡AIbus
+
+## 安装chrome
+
+```shell
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get update
+sudo apt install ./google-chrome-stable_current_amd64.deb
+```
+
+- 验证是否安装成功
+
+```shell
+google-chrome --version
+```
+
+## 安装chromedriver
+
+- chromedriver 对应 google chrome 第一个大数字就行，比如 `119` 。
+
+```shell
+wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/{google chrome 版本号}/linux64/chromedriver-linux64.zip
+unzip chromedriver-linux64.zip;
+sudo mv ./chromedriver-linux64/chromedriver /usr/bin/
+```
+
+- 验证一下
+
+```shell
+chromedriver --version
+```
+
+## 安装依赖库
+
+```shell
+pip3 install -r requirements.txt
+```
+
+## 克隆项目
+
+```shell
+git clone https://github.com/PKUColdkeyboard/AIBUS-tool.git
+```
+
+## 配置文件
+### config.ini
+- 用于登录 AIBUS 网站的账号与密码
+
+### apikey.json
+请首先将 apikey.sample.json 复制一份并改名为 apikey.json，并按照以下说明进行配置。
+
+该文件为 [TT识图](http://www.ttshitu.com/) 平台的 API 密钥，在平台注册后，填入用户名与密码即可。由于该 API 需要收费，须在平台充值后方可使用（1 RMB 足够用到天荒地老了，实际测试识别一次计算题验证码需要 0.008 RMB）。
+
+```json
+{
+    "username": "yourusername",
+    "password": "yourpassword"
+}
+```
+
+
+## 运行项目
+
+```shell
+mv config.example.ini config.ini
+mv apikey.sample.json apikey.json
+
+# 然后修改 config.ini 为自己账号，修改 apikey.json 为自己在 TT 识图上注册的账号
+
+python3 main.py
+```
+
+## 定时运行
+
+- 自行查阅 `crontab` 用法，只要项目目录中有 `config.ini` 这个文件，然后设置执行 `python3 main.py` 就行。
+- 要支持多账号签到，提前创建几个文件如 `config.ini.1` ，`config.ini.2` 等，每个文件放入一个账号，然后定时的时候加一行 `mv config.ini.1 config.ini` ，有几个账号就重复几次 `mv` 和 `python3 main.py` 的步骤。
--- func.py
+++ func.py
@@ -4,7 +4,10 @@ import time
 import traceback
 import warnings
 
+from PIL import Image
 from selenium.webdriver.common.by import By
+
+from captcha import base64_api
 warnings.filterwarnings('ignore')
 
 
@@ -16,7 +19,7 @@ def login(driver, username, password, retry=0):
     url = 'https://www.aibusx.com/pages/login'
     driver.get(url)
     # WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'MuiButton-sizeLarge')))
-    time.sleep(5)
+    time.sleep(3)
     driver.find_element(By.ID, 'email').send_keys(username)
     time.sleep(0.1)
     driver.find_element(By.ID, 'auth-login-password').send_keys(password)
@@ -34,7 +37,7 @@ def login(driver, username, password, retry=0):
 # 签到
 def checkin(driver):
     try:
-        time.sleep(10)
+        time.sleep(3)
         button = driver.find_elements(
             By.CLASS_NAME, 'MuiButton-textCheckinBtn')[0]
         print(button.get_dom_attribute('class'))
@@ -43,7 +46,41 @@ def checkin(driver):
             button.click()
         else:
             raise RuntimeError('Button is disabled or not clickable')
-        time.sleep(3)
+        time.sleep(5)
+
+        # 获取网页截图
+        driver.save_screenshot('screenshot.png')
+
+        window_size = driver.get_window_size()
+        center_x = window_size['width'] / 2
+        center_y = window_size['height'] / 2
+        width = 150
+        height = 50
+
+        left = center_x - width / 2
+        top = center_y - height
+        right = center_x + width / 2
+        bottom = center_y + 20
+
+        # 裁剪验证码
+        im = Image.open('screenshot.png')
+        im = im.crop((int(left), int(top), int(right), int(bottom)))
+        im.save('captcha.png')
+
+        with open('apikey.json') as f:
+            apikey = json.load(f)
+        uname = apikey['username']
+        pwd = apikey['password']
+        captcha_code = base64_api(
+            uname=uname, pwd=password, img='captcha.png', typeid=11)
+        print(f'验证码答案：{captcha_code}')
+        driver.find_element(
+            By.XPATH, '/html/body/div[2]/div[3]/div/div[1]/div/input').send_keys(captcha_code)
+        driver.find_element(
+            By.XPATH, '/html/body/div[2]/div[3]/div/div[2]/button[2]').click()
+
+        time.sleep(2)
+
         print('签到成功！')
     except Exception as e:
         print('签到失败！')
